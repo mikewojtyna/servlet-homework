8ca80e3827584eca07cd88419450351f46c92969
Mike Wojtyna <mike@buildmysoftware.pro>
2018-10-27 11:02:16 +0200

    Add filters order task solution

---
+++ src/main/java/pl/sdacademy/servlet/Filter1.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Filter1 implements Filter {
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+		chain.doFilter(req, resp);
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+
+	}
+
+}
